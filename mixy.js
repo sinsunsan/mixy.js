f88497215d0ecdfb6c50a70469e21a3baedb5b4c
sinsunsan <lucas.sebastien@gmail.com>
2013-09-13 12:14:24 +0200

    test

--- mixy.js
+++ mixy.js
@@ -1,4 +1,6 @@
 var mixy = {
+  //http://html5hub.com/ellipse-my-text/#i.7ruh141bulev7t
+  // Good but make assumption of the space available to display the text
   textShorten: function(text, maxLength) {
       var ret = text;
       if (ret.length > maxLength) {
@@ -6,5 +8,15 @@ var mixy = {
       }
       return ret;  
   },
+  test: function(){
+    mixyTest.init();
+  }
+}
+var mixyTest = {
+  init: function(){
+    var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eros lorem, ultrices eget augue at, accumsan dictum ligula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur egestas elit sit amet consectetur condimentum. Nullam cursus tortor eu turpis gravida auctor.';
+    return mixy.textShorten(text, 100);
+    
+  }
 }
 console.log('mixy utiliy loaded !');
